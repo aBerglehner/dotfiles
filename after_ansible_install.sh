6e2d804fdf34409a06c1696cdc08465ddb0a908a
aBerglehner <alexanderberglehner2@gmail.com>
2024-05-23 01:30:47 +0200

    feat: added zoxide to install script with cargo due to problems with curl download in ansible

--- after_ansible_install.sh
+++ after_ansible_install.sh
@@ -3,6 +3,9 @@
 # install atuin
 cargo install atuin
 
+# install zoxide
+cargo install zoxide --locked
+
 # install fzf
 apt remove fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
