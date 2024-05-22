b4ad2366c17a7ec9907c040ec76ee86fde7a509e
aBerglehner <alexanderberglehner2@gmail.com>
2024-05-23 01:44:12 +0200

    chore: update

--- after_ansible_install.sh
+++ after_ansible_install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
+# install fzf
+apt remove fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install | sh -s -- -y
+
 # install atuin
 cargo install atuin
 
 # install zoxide
 cargo install zoxide --locked
-
-# install fzf
-apt remove fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
