f493d5c571b7949ff0805f0323086a0e40b97b7f
aBerglehner <alexanderberglehner2@gmail.com>
2024-06-27 23:33:18 +0200

    chore: adjusted comment for after_ansible_install

--- after_ansible_install.sh
+++ after_ansible_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # install fzf
-# yes no yes
+# yes no yes is the sequence when asked
 echo 'install fzf -> yes no yes'
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
